3f32a7d022f714cd8eb671aa47c5e2211cee47b8
Anonymous <user@rivne.itstep.org>
2019-05-22 19:58:28 +0200

    index change

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME','telesyk');
+define('DB_USER','telesyk_user');
+define('DB_PASSWORD','CKBHVz2fZbiT6Jnk');
+define('DB_HOST','localhost');
+define('DB_CHARSET','utf8');
+?>
--- index.php
+++ index.php
@@ -1,8 +1,22 @@
+<?php include("connection_database.php") ?>
 <?php include_once("header.php") ?>
 
-    <h1>Home Page</h1>
+   <table class="table">
     <?php
-        echo "------Hello-----";
-    ?>
+//$sss = 11;
+    $res = $connect->query("SELECT Id,Name,Description FROM tbl_roles");
+    $num = 1;
+    while ($row = $res->fetch_assoc()) {
+    echo "
+    <tr>
+    <td>{$num}</td>
+    <td>" . $row['Name'] . "</td>
+    <td>{$row['Description']}</td>
+    </tr>
+    ";
+    $num++;
+    }
+?>
+</table>
 
 <?php include_once("footer.php") ?>
